85416e0cd9c88c4f009d4e123a02b49d739ef12b
Jon Herbst <herbtaculargames@gmail.com>
2020-08-17 11:37:46 +0100

    Updating root window title on for each button click

--- main_app.py
+++ main_app.py
@@ -8,6 +8,8 @@ class MainApp(tk.Frame):
         self.master = master
         self.main = main.MainMenu(self)
 
+        self.window_title = 'Life Dashboard'
+
         self.main.pack()
         self.pack()
 
@@ -15,17 +17,29 @@ class MainApp(tk.Frame):
         self.clear_view()
 
         if 'menu' == view:
+            self.master.title('Menu - ' + self.window_title)
+
             self.main = main.MainMenu(self)
             self.main.pack()
         elif 'budget' == view:
+            self.master.title('Budget - ' + self.window_title)
+
             self.budget = ''
         elif 'xeffect' == view:
+            self.master.title('X-Effect - ' + self.window_title)
+            
             self.xeffect = ''
         elif 'goals' == view:
+            self.master.title('Goal Setting - ' + self.window_title)
+
             self.goals = ''
         elif 'schedule' == view:
+            self.master.title('Schedule - ' + self.window_title)
+
             self.schedule = ''
         elif 'journal' == view:
+            self.master.title('Journal - ' + self.window_title)
+
             self.journal = ''
 
 
@@ -35,7 +49,7 @@ class MainApp(tk.Frame):
 
 if '__main__' == __name__:
     root = tk.Tk()
-    root.title('Life Dashboard')
+    root.title('Menu - Life Dashboard')
     root.geometry('1280x720')
     root.resizable(width=False, height=False)
 

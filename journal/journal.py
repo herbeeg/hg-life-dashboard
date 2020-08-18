80a6906897768ebbc6a3cb15962bd2b2a923c0ed
Jon Herbst <herbtaculargames@gmail.com>
2020-08-18 11:54:22 +0100

    Creating save/load file dialogs and printing out contents of tkinter Text input to the command line

--- journal/journal.py
+++ journal/journal.py
@@ -1,3 +1,4 @@
+import tkinter.filedialog
 import tkinter as tk
 
 class Journal(tk.Frame):
@@ -5,6 +6,7 @@ class Journal(tk.Frame):
         super().__init__(master)
         self.master = master
 
+        self.frame_padding = 5
         self.button_padding = 50
 
         self.create_widgets()
@@ -15,12 +17,24 @@ class Journal(tk.Frame):
 
         self.journal_delete = tk.Button(self, padx=self.button_padding)
         self.journal_delete['text'] = 'Delete'
-        self.journal_delete.pack(side='right', fill='x')
+        self.journal_delete.pack(side='right', fill='x', padx=self.frame_padding, pady=self.frame_padding)
 
-        self.journal_save = tk.Button(self, padx=self.button_padding)
+        self.journal_save = tk.Button(self, padx=self.button_padding, command=self.save_file)
         self.journal_save['text'] = 'Save'
-        self.journal_save.pack(side='right', fill='x')
+        self.journal_save.pack(side='right', fill='x', padx=self.frame_padding, pady=self.frame_padding)
 
-        self.journal_load = tk.Button(self, padx=self.button_padding)
+        self.journal_load = tk.Button(self, padx=self.button_padding, command=self.pick_file)
         self.journal_load['text'] = 'Load'
-        self.journal_load.pack(side='right', fill='x')
+        self.journal_load.pack(side='right', fill='x', padx=self.frame_padding, pady=self.frame_padding)
+
+    def pick_file(self):
+        filename = tk.filedialog.askopenfilename(title='Select Journal', filetypes=[('Text Files', '*.txt')])
+
+    def save_file(self):
+        filename = tk.filedialog.asksaveasfilename(title='Save Journal', filetypes=[('Text Files', '*.txt')])
+
+        contents = self.get_contents()
+        print(contents)
+
+    def get_contents(self):
+        return self.journal_input.get(1.0, 'end-1c')

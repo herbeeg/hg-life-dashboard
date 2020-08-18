16768915be498ecadb8310d4f4b189ea9c61b9ca
Jon Herbst <herbtaculargames@gmail.com>
2020-08-18 11:28:44 +0100

    Adding buttons below text area input to map journal actions to

--- journal/journal.py
+++ journal/journal.py
@@ -5,8 +5,22 @@ class Journal(tk.Frame):
         super().__init__(master)
         self.master = master
 
+        self.button_padding = 50
+
         self.create_widgets()
 
     def create_widgets(self):
         self.journal_input = tk.Text(self, height=20, borderwidth=50)
-        self.journal_input.pack(side='top', fill='x')
+        self.journal_input.pack(side='top', fill='both')
+
+        self.journal_delete = tk.Button(self, padx=self.button_padding)
+        self.journal_delete['text'] = 'Delete'
+        self.journal_delete.pack(side='right', fill='x')
+
+        self.journal_save = tk.Button(self, padx=self.button_padding)
+        self.journal_save['text'] = 'Save'
+        self.journal_save.pack(side='right', fill='x')
+
+        self.journal_load = tk.Button(self, padx=self.button_padding)
+        self.journal_load['text'] = 'Load'
+        self.journal_load.pack(side='right', fill='x')

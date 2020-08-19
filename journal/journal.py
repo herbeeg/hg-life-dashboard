f13d1ae1cb75ba73578220dedafe63c2ecc9718a
Jon Herbst <herbtaculargames@gmail.com>
2020-08-19 19:54:45 +0100

    Python documentation for journal window class

--- journal/journal.py
+++ journal/journal.py
@@ -4,7 +4,21 @@ import tkinter as tk
 import os
 
 class Journal(tk.Frame):
+    """
+    Builds all of the journal frame elements and
+    contains logic for text file manipulation
+    when dealing with journal entries.
+
+    Extends the tkinter Frame class.
+    """
     def __init__(self, master=None):
+        """
+        Set GUI element padding values and animation
+        settings for the status frame.
+
+        Args:
+            master (Tk, optional): The parent tkinter window element. Defaults to None.
+        """
         super().__init__(master)
         self.master = master
 
@@ -15,6 +29,15 @@ class Journal(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
+        """
+        Render all journal widget elements and map 
+        all input commands to their respective 
+        filedialog functions.
+
+        The status label doesn't get packed as we
+        don't want to render that on the screen
+        until we've had a file operation.
+        """
         self.journal_input = tk.Text(self, height=20, borderwidth=50)
         self.journal_input.pack(side='top', fill='both')
 
@@ -32,9 +55,19 @@ class Journal(tk.Frame):
 
         self.journal_status = tk.Label(self, bg='#c9f6c9')
         self.journal_status['text'] = ''
-        
 
     def pick_file(self):
+        """
+        Generate a filedialog window when the 'Edit'
+        button is clicked and attempt to read
+        any text files that are selected.
+
+        Uses the 'r+' mode so that respective files
+        are opened for reading and writing.
+
+        Raises:
+            TypeError: Fail if the file extension is not a valid text file
+        """
         filename, file_extension = os.path.splitext(tk.filedialog.askopenfilename(title='Edit Journal', filetypes=[('Text Files', '*.txt')]))
 
         if filename:
@@ -51,6 +84,14 @@ class Journal(tk.Frame):
                 tk.messagebox.showerror(title='Error Loading Journal', message='Unable to open file %s' % filename)
 
     def save_file(self):
+        """
+        Generate a filedialog window when the 'Save'
+        button is clicked and allow the user to
+        specify a filename and location.
+
+        Uses the 'w+' mode so that respective files
+        are cleared before writing takes place.
+        """
         filename = tk.filedialog.asksaveasfilename(title='Save Journal', filetypes=[('Text Files', '*.txt')])
 
         if filename:
@@ -62,6 +103,19 @@ class Journal(tk.Frame):
             self.update_status('File Saved!')
 
     def delete_file(self):
+        """
+        Uses the pre-existing askopenfilename() function
+        to generate a window where the user can choose
+        a file to delete by 'opening'.
+
+        Users are required to confirm that they want
+        to delete the chosen file again before
+        the application can proceed.
+
+        Raises:
+            TypeError: Fail if the file extension is not a valid text file
+            IOError: Fail if the file does not exist
+        """
         filename, file_extension = os.path.splitext(tk.filedialog.askopenfilename(title='Delete Journal', filetypes=[('Text Files', '*.txt')]))
 
         if filename:
@@ -82,18 +136,54 @@ class Journal(tk.Frame):
                     tk.messagebox.showerror(title='Error Deleting Journal', message='Could not delete file %s' % filename)
 
     def get_contents(self):
+        """
+        Gets the entirity of data from the text file
+        by specifying '1.0' as the starting point.
+
+        Avoids any trailing newline characters by
+        specifying 'end-1c', meaning up to the
+        last but one character.
+
+        Returns:
+            str: The contents of the text file
+        """
         return self.journal_input.get(1.0, 'end-1c')
 
     def refresh_text_input(self, content):
+        """
+        Completely clear the text input area before
+        text file contents are loaded for avoid
+        any concatenation.
+
+        Args:
+            content (str): The text file contents
+        """
         self.journal_input.delete(1.0, 'end-1c')
         self.journal_input.insert(1.0, content)
 
     def update_status(self, text):
+        """
+        Change the relevant text on the label
+        element and un-hide it by packing
+        it once again.
+
+        Args:
+            text (str): Text to be printed on the label
+        """
         self.journal_status['text'] = text
         self.after(self.animation_delay, self.animate_status)
         self.journal_status.pack(side='left', fill='x', expand=True, padx=self.frame_padding, pady=self.frame_padding)
 
     def animate_status(self):
+        """
+        Recursive function that removes the last
+        character from the label string each
+        time it's called until none are left.
+
+        After the initial wait period passes, a
+        shorter time period is used to give
+        the feeling of an animation.
+        """
         current_text = self.journal_status['text']
 
         if 0 < len(current_text):

327c5cdf1f9cc7d3c1bdc6d7f24d42795c2bc902
Jon Herbst <herbtaculargames@gmail.com>
2020-08-26 18:34:03 +0100

    Generate padded grid schedule area with each day and hour displayed to test

--- schedule/schedule_generator.py
+++ schedule/schedule_generator.py
@@ -21,12 +21,24 @@ class ScheduleGenerator(tk.Frame):
             self.row_index += 1
 
         self.row_index = 0
+        self.col_index += 1
         
         for day in self.column_titles():
-            for hour in list(range(24)):
-                return True
+            day_title = tk.Label(self, width=10, padx=self.frame_padding)
+            day_title['text'] = day
+            day_title.grid(row=self.row_index, column=self.col_index, sticky='NSEW')
 
-        return True
+            self.row_index += 1
+
+            for hour in list(range(24)):
+                hour_area = tk.Label(self, borderwidth=2, relief='groove', pady=10)
+                hour_area['text'] = day + ' ' + str(hour)
+                hour_area.grid(row=self.row_index, column=self.col_index, sticky='NSEW')
+
+                self.row_index += 1
+            
+            self.row_index = 0
+            self.col_index += 1
 
     def column_titles(self):
         return [

a556835e00c55e08e11f526e02c94611e7901ed2
Jon Herbst <herbtaculargames@gmail.com>
2020-08-26 16:33:11 +0100

    Add generate button and reduce padding between dropdown elements

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -6,6 +6,7 @@ class Schedule(tk.Frame):
         self.master = master
 
         self.frame_padding = 10
+        self.button_padding = 30
 
         self.create_widgets()
 
@@ -20,17 +21,21 @@ class Schedule(tk.Frame):
 
         self.end_time_label = tk.Label(self)
         self.end_time_label['text'] = 'End Time:'
-        self.end_time_label.pack(side='left', padx=self.frame_padding, pady=self.frame_padding)
+        self.end_time_label.pack(side='left', pady=self.frame_padding)
 
         self.end_time_input = tk.OptionMenu(self, self.day_end, *hours)
         self.end_time_input.pack(side='left', padx=self.frame_padding, pady=self.frame_padding)
 
         self.start_time_label = tk.Label(self)
         self.start_time_label['text'] = 'Start Time:'
-        self.start_time_label.pack(side='left', padx=self.frame_padding, pady=self.frame_padding)
+        self.start_time_label.pack(side='left', pady=self.frame_padding)
 
         self.start_time_input = tk.OptionMenu(self, self.day_start, *hours)
         self.start_time_input.pack(side='left', padx=self.frame_padding, pady=self.frame_padding)
+
+        self.generate_button = tk.Button(self, padx=self.button_padding, command=self.generate_week_schedule)
+        self.generate_button['text'] = 'Generate'
+        self.generate_button.pack(side='left', padx=self.frame_padding, pady=self.frame_padding)
 
         return True
 

af09a8bc4709d0a359c4df884a00b74735e87b0b
Jon Herbst <herbtaculargames@gmail.com>
2020-08-28 18:42:21 +0100

    Generating uniform width columns to manage spacing between each goal section

--- goals/goal_setting.py
+++ goals/goal_setting.py
@@ -4,3 +4,28 @@ class GoalSetting(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.goal_1_button = tk.Button(self, command=self.generate_goal_layout)
+        self.goal_1_button['text'] = 'New Goal'
+        self.goal_1_button.grid(row=0, column=1, sticky='NSEW')
+
+        self.goal_2_button = tk.Button(self, command=self.generate_goal_layout)
+        self.goal_2_button['text'] = 'New Goal'
+        self.goal_2_button.grid(row=0, column=4, sticky='NSEW')
+
+        self.goal_3_button = tk.Button(self, command=self.generate_goal_layout)
+        self.goal_3_button['text'] = 'New Goal'
+        self.goal_3_button.grid(row=0, column=7, sticky='NSEW')
+
+        self.goal_4_button = tk.Button(self, command=self.generate_goal_layout)
+        self.goal_4_button['text'] = 'New Goal'
+        self.goal_4_button.grid(row=0, column=10, sticky='NSEW')
+
+        for col in range(12):
+            self.grid_columnconfigure(col, weight=1, uniform='goals')
+
+    def generate_goal_layout(self):
+        return True

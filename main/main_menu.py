f09287ccb2c30ae396a47a973dc0f7dc2e8a5c75
Jon Herbst <herbtaculargames@gmail.com>
2020-08-28 11:19:54 +0100

    Alphabetical re-ordering of main menu buttons

--- main/main_menu.py
+++ main/main_menu.py
@@ -36,18 +36,18 @@ class MainMenu(tk.Frame):
         to allow passing of parameters to button
         command configs.
         """
-        self.xeffect_open = tk.Button(self, padx=self.button_padding, command=partial(self.master.load_view, 'xeffect'))
-        self.xeffect_open['text'] = 'X-Effect'
-        self.xeffect_open.pack(side='top', fill='x', padx=self.frame_padding, pady=self.frame_padding)
-
         self.goals_open = tk.Button(self, padx=self.button_padding, command=partial(self.master.load_view, 'goals'))
         self.goals_open['text'] = 'Goal Setting'
         self.goals_open.pack(side='top', fill='x', padx=self.frame_padding, pady=self.frame_padding)
+
+        self.journal_open = tk.Button(self, padx=self.button_padding, command=partial(self.master.load_view, 'journal'))
+        self.journal_open['text'] = 'Journal'
+        self.journal_open.pack(side='top', fill='x', padx=self.frame_padding, pady=self.frame_padding)
 
         self.schedules_open = tk.Button(self, padx=self.button_padding, command=partial(self.master.load_view, 'schedule'))
         self.schedules_open['text'] = 'Schedule'
         self.schedules_open.pack(side='top', fill='x', padx=self.frame_padding, pady=self.frame_padding)
 
-        self.journal_open = tk.Button(self, padx=self.button_padding, command=partial(self.master.load_view, 'journal'))
-        self.journal_open['text'] = 'Journal'
-        self.journal_open.pack(side='top', fill='x', padx=self.frame_padding, pady=self.frame_padding)
+        self.xeffect_open = tk.Button(self, padx=self.button_padding, command=partial(self.master.load_view, 'xeffect'))
+        self.xeffect_open['text'] = 'X-Effect'
+        self.xeffect_open.pack(side='top', fill='x', padx=self.frame_padding, pady=self.frame_padding)

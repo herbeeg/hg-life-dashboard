fe1613d90e02dcdd60026e1be140183af57fad4e
Jon Herbst <herbtaculargames@gmail.com>
2020-08-28 12:10:06 +0100

    Adding full documentation for Schedule() and ScheduleGenerator() classes

--- schedule/schedule.py
+++ schedule/schedule.py
@@ -6,7 +6,23 @@ import tkinter as tk
 from .schedule_generator import ScheduleGenerator
 
 class Schedule(tk.Frame):
+    """
+    Houses the CRUD elements when it comes to
+    making modifications to the generated
+    schedule grid through mapping of
+    the generation inputs.
+
+    Extends the tkinter Frame class.
+    """
     def __init__(self, master=None):
+        """
+        Set the GUI padding values for the schedule
+        generation inputs and directory values
+        when attempting to load saved data.
+
+        Args:
+            master (Tk, optional): The parent tkinter window element. Defaults to None.
+        """
         super().__init__(master)
         self.master = master
 
@@ -18,6 +34,12 @@ class Schedule(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
+        """
+        Render all of the generation input options
+        in a separate frame, allowing us to
+        utilise both the pack() and grid()
+        functions in the same window.
+        """
         hours = list(range(24))
 
         self.generation_options = tk.Frame(self)
@@ -27,12 +49,14 @@ class Schedule(tk.Frame):
 
         self.day_end = tk.IntVar(self.master)
         self.day_end.set(0)
+        """Allow tracking of both dropdown input values for reference when a schedule is generated."""
 
         self.start_time_label = tk.Label(self.generation_options)
         self.start_time_label['text'] = 'Start Time:'
         self.start_time_label.pack(side='left', pady=self.frame_padding)
 
         self.start_time_input = tk.OptionMenu(self.generation_options, self.day_start, *hours)
+        """The asterisk on the hours variable allows us to unpack the list when used as an argument."""
         self.start_time_input.pack(side='left', padx=self.frame_padding, pady=self.frame_padding)
 
         self.end_time_label = tk.Label(self.generation_options)
@@ -51,15 +75,26 @@ class Schedule(tk.Frame):
         self.save_button.pack(side='left', padx=self.frame_padding, pady=self.frame_padding)
 
         self.generation_options.pack(side='top')
+        """The options are stuck to the top of the window to ensure the schedule grid renders on the next line(s)."""
 
         self.json_data = self.maybe_load_schedule()
 
     def generate_week_schedule(self, from_file=False):
+        """
+        Instantiate a new ScheduleGenerator() object with local
+        JSON data if a valid file exists in the requisite
+        directory.
+
+        Args:
+            from_file (bool, optional): True if we want to store loaded data on startup. Defaults to False.
+        """
         if hasattr(self, 'schedule_grid'):
             self.clear_schedule()
+            """Only attempt to clear the grid if the frame already exists in the window."""
 
         self.schedule_grid = ScheduleGenerator(self, self.day_start.get(), self.day_end.get(), self.json_data)
         self.schedule_grid.pack()
+        """The schedule is packed separately as it uses the grid() function which is incompatible with pack() when used in the same frame."""
 
         if from_file:
             self.schedule_grid.store_data(json_string=self.json_data)
@@ -87,6 +122,13 @@ class Schedule(tk.Frame):
         self.schedule_grid.store_data(json)
 
     def save_schedule(self):
+        """
+        Write encoded JSON to a specified directory and
+        filename whenever the save button is clicked.
+
+        Raises:
+            AttributeError: Fail if the grid frame does not already exist in the window
+        """
         try:
             data = self.schedule_grid.get_data()
 
@@ -101,11 +143,32 @@ class Schedule(tk.Frame):
             tk.messagebox.showerror(title='Error Saving Data', message='Grid schedule has not been generated yet.')
 
     def maybe_load_schedule(self):
+        """
+        If a valid local JSON file exists, then we use 
+        that to generate a new schedule grid with 
+        the specified start/end times and task
+        names.
+
+        Otherwise, we return an empty dict object so that 
+        the schedule generator knows to scaffold a new 
+        dict object with empty data for editing when
+        the user clicks the generate button.
+
+        Raises:
+            FileNotFoundError: If the specific named file does not exist
+
+        Raises:
+            TypeError: If the file does not have the '.json' extension
+
+        Returns:
+            dict: Empty dict object if no valid file exists
+        """
         try:
             with open(self.master.get_working_directory() + self.data_directory) as file:
                 filename, file_extension = os.path.splitext(file.name)
         except FileNotFoundError:
             tk.messagebox.showwarning(title='Couldn\'t Load Data', message='No valid file found.')
+            """The user can continue to generate a new schedule grid from here as no local data is required."""
             
             return {}
 
@@ -127,4 +190,10 @@ class Schedule(tk.Frame):
                 tk.messagebox.showerror(title='Error Loading Data', message='Unable to open file %s' % filename)
 
     def clear_schedule(self):
+        """
+        When a new schedule grid is generated, we want to
+        clear any existing frames in the window before
+        going ahead, preventing multiple grids in
+        memory at one time.
+        """
         self.schedule_grid.destroy()
--- schedule/schedule_generator.py
+++ schedule/schedule_generator.py
@@ -3,7 +3,29 @@ import tkinter as tk
 from functools import partial
 
 class ScheduleGenerator(tk.Frame):
+    """
+    Handles widget generation of the schedule grid,
+    the size and values of which are decided
+    either by user input or configurations
+    housed in a local JSON file.
+
+    Extends the tkinter Frame class.
+    """
     def __init__(self, master=None, hour_start=0, hour_end=23, loads={}):
+        """
+        If local JSON data exists, then we immediately 
+        store that information in a class variable.
+
+        Otherwise, we want to setup a new dict object
+        with empty values so that we're able to
+        access the variable in the same way.
+
+        Args:
+            master (Tk, optional): The parent tkinter window element. Defaults to None.
+            hour_start (int, optional): Time to start the schedule grid from. Defaults to 0.
+            hour_end (int, optional): Time to finish the schedule grid from. Defaults to 23.
+            loads (dict, optional): Local JSON data if it exists. Defaults to {}.
+        """
         super().__init__(master)
         self.master = master
 
@@ -32,6 +54,18 @@ class ScheduleGenerator(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
+        """
+        Render the schedule grid as clickable Label text 
+        containers, that allow the user to edit
+        existing elements, save those changes
+        to a file or discard as necessary.
+
+        Click events are bound to a left-click on each
+        generated label and allow us to track them
+        individually by passing parameters to
+        the callback function instead of
+        storing them in variables.
+        """
         for hour in list(range(self.hour_start, self.hour_end)):
             hour_display = tk.Label(self)
             hour_display['text'] = hour
@@ -63,6 +97,14 @@ class ScheduleGenerator(tk.Frame):
             self.col_index += 1
 
     def column_titles(self):
+        """
+        We convert these values to title case when
+        printing them out at the top of each
+        column in the schedule grid.
+
+        Returns:
+            list: Lower case versions of each weekday.
+        """
         return [
             'monday',
             'tuesday',
@@ -74,7 +116,22 @@ class ScheduleGenerator(tk.Frame):
         ]
 
     def store_data(self, json_string):
+        """
+        We use this data locally when making edit
+        operations to the schedule grid and
+        encoding to an output file.
+
+        Args:
+            json_string (str): Condensed JSON data
+        """
         self.grid_data = json_string
 
     def get_data(self):
+        """
+        Getter for readability when the Schedule()
+        class makes any references.
+
+        Returns:
+            str: Condensed JSON data
+        """
         return self.grid_data

b9eb572ab42ac1f4a617ed3c9eb547fe8cc7fc0e
Jon Herbst <herbtaculargames@gmail.com>
2020-08-30 18:25:42 +0100

    Goal column modifications to swap out existing button for edit button and text labels

--- goals/goal_setting.py
+++ goals/goal_setting.py
@@ -14,21 +14,26 @@ class GoalSetting(tk.Frame):
         self.top_padding = tk.Label(self, text='')
         self.top_padding.grid(row=0, column=0, columnspan=12, pady=10)
 
-        self.goal_1_button = tk.Button(self, command=partial(self.generate_goal_layout, col_index=0))
-        self.goal_1_button['text'] = 'New Goal'
-        self.goal_1_button.grid(row=1, column=1, sticky='NSEW')
+        button_text = 'New Goal'
 
-        self.goal_2_button = tk.Button(self, command=partial(self.generate_goal_layout, col_index=1))
-        self.goal_2_button['text'] = 'New Goal'
-        self.goal_2_button.grid(row=1, column=4, sticky='NSEW')
+        self.goal_1_new = tk.Button(self, command=partial(self.generate_goal_layout, col_index=1))
+        self.goal_1_new['text'] = button_text
+        self.goal_1_new.grid(row=1, column=1, sticky='NSEW')
 
-        self.goal_3_button = tk.Button(self, command=partial(self.generate_goal_layout, col_index=2))
-        self.goal_3_button['text'] = 'New Goal'
-        self.goal_3_button.grid(row=1, column=7, sticky='NSEW')
+        self.goal_2_new = tk.Button(self, command=partial(self.generate_goal_layout, col_index=4))
+        self.goal_2_new['text'] = button_text
+        self.goal_2_new.grid(row=1, column=4, sticky='NSEW')
 
-        self.goal_4_button = tk.Button(self, command=partial(self.generate_goal_layout, col_index=3))
-        self.goal_4_button['text'] = 'New Goal'
-        self.goal_4_button.grid(row=1, column=10, sticky='NSEW')
+        self.goal_3_new = tk.Button(self, command=partial(self.generate_goal_layout, col_index=7))
+        self.goal_3_new['text'] = button_text
+        self.goal_3_new.grid(row=1, column=7, sticky='NSEW')
+
+        self.goal_4_new = tk.Button(self, command=partial(self.generate_goal_layout, col_index=10))
+        self.goal_4_new['text'] = button_text
+        self.goal_4_new.grid(row=1, column=10, sticky='NSEW')
+
+        self.bot_padding = tk.Label(self, text='')
+        self.bot_padding.grid(row=2, column=0, columnspan=12, pady=10)
 
         for col in range(12):
             self.grid_columnconfigure(col, weight=1, uniform='goals')
@@ -36,4 +41,28 @@ class GoalSetting(tk.Frame):
     def generate_goal_layout(self, col_index):
         self.goal_dialog = GoalDialog(self.master)
         """Wait for user to fill in the dialog options or cancel the operation."""
-        print(self.goal_dialog.get_goal_config())
+
+        button_text = 'Edit Goal'
+
+        if 1 == col_index:
+            self.goal_1_new.destroy()
+            self.goal_1_edit = tk.Button(self, command=partial(self.edit_goal_layout, col_index=1))
+            self.goal_1_edit['text'] = button_text
+            self.goal_1_edit.grid(row=1, column=0, columnspan=3)
+        elif 4 == col_index:
+            self.goal_2_button.destroy()
+        elif 7 == col_index:
+            self.goal_3_button.destroy()
+        elif 10 == col_index:
+            self.goal_4_button.destroy()
+
+        self.grid_columnconfigure(col_index-1, weight=1, uniform='')
+        self.grid_columnconfigure(col_index, weight=1, uniform='')
+        self.grid_columnconfigure(col_index+1, weight=1, uniform='')
+        
+        label = tk.Label(self)
+        label['text'] = 'Goal 1: ' + self.goal_dialog.get_goal_config()['name']
+        label.grid(row=3, column=0, columnspan=3)
+
+    def edit_goal_layout(self, col_index):
+        return True

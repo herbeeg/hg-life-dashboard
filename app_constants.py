c30c004c7847d37e27d37c5ca671c51d2a0846d1
Jon Herbst <herbtaculargames@gmail.com>
2020-09-02 17:22:26 +0100

    AppConstants class documentation

--- app_constants.py
+++ app_constants.py
@@ -1,4 +1,15 @@
 class AppConstants:
+    """
+    Houses any data that could be used in
+    multiple areas of the application.
+    """
     @staticmethod
     def filePreface():
+        """
+        Preface for JSON file used for
+        x-effect data storage.
+
+        Returns:
+            str: File preface for the x-effect data loading
+        """
         return 'ld_'
